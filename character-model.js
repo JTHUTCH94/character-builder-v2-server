487162b631f8a70045806634acbd95259d715853
Joshua Hutchinson <jthutch94@gmail.com>
2018-10-11 13:40:24 -0400

    made small changes to naming schemes in character model

--- character-model.js
+++ character-model.js
@@ -4,10 +4,10 @@ const mongoose = require('mongoose');
 
 const characterSchema = new mongoose.Schema({
   //userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  name: { type: 'String', unique: true, required: true},
-  race: { type: 'String', required: true},
-  vocation: { type: 'String', required: true},
-  weapon: { type: 'String', required: true}
+  currentName: { type: 'String', unique: true, required: true},
+  currentRace: { type: 'String', required: true},
+  currentVocation: { type: 'String', required: true},
+  currentWeapon: { type: 'String', required: true}
 });
 
 characterSchema.set('toObject', {
